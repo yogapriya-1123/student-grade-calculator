5f612802048509c55eb000876b2f0d51dc8e6c7f
Yoga Priya D <yogapriya1123@gmail.com>
2026-08-28 08:19:35 -0700

    Complete student grade calculator

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,1 +1,22 @@
 name = input("Enter student name:")
+maths = int(input("Enter Maths marks:"))
+science = int(input("Enter Science marks:"))
+english = int(input("Enter English marks: "))
+
+total = maths + science + english
+average = total / 3
+
+print("Student Name:", name)
+print("Total Marks:", total)
+print("Average:", average)
+
+if average >= 90:
+    print("Grade: A")
+elif average >= 75:
+    print("Grade: B")
+elif average >= 60:
+    print("Grade: C")
+elif average >= 50:
+    print("Grade: D")
+else:
+    print("Grade: F")
