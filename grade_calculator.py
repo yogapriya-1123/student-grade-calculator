b50e557c579d441edbc97bec299fbc826a0b8af4
Yoga Priya D <yogapriya1123@gmail.com>
2026-08-28 08:54:31 -0700

    Add marks validation

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,22 +1,26 @@
-name = input("Enter student name:")
-maths = int(input("Enter Maths marks:"))
-science = int(input("Enter Science marks:"))
+name = input("Enter student name: ")
+
+maths = int(input("Enter Maths marks: "))
+science = int(input("Enter Science marks: "))
 english = int(input("Enter English marks: "))
 
-total = maths + science + english
-average = total / 3
-
-print("Student Name:", name)
-print("Total Marks:", total)
-print("Average:", average)
-
-if average >= 90:
-    print("Grade: A")
-elif average >= 75:
-    print("Grade: B")
-elif average >= 60:
-    print("Grade: C")
-elif average >= 50:
-    print("Grade: D")
+if not (0 <= maths <= 100 and 0 <= science <= 100 and 0 <= english <= 100):
+    print("Invalid marks! Marks must be between 0 and 100.")
 else:
-    print("Grade: F")
+    total = maths + science + english
+    average = total / 3
+
+    print("Student Name:", name)
+    print("Total Marks:", total)
+    print("Average:", average)
+
+    if average >= 90:
+        print("Grade: A")
+    elif average >= 75:
+        print("Grade: B")
+    elif average >= 60:
+        print("Grade: C")
+    elif average >= 50:
+        print("Grade: D")
+    else:
+        print("Grade: F")
